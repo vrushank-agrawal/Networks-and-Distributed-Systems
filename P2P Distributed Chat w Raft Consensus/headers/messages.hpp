aa3036c6aef4ccb633ff5b1b2ff285cb2deb7994
Vrushank Agrawal <vrushank2001@gmail.com>
2024-05-07 19:20:55 -0400

    add uncommitted files

--- P2P Distributed Chat w Raft Consensus/headers/messages.hpp
+++ P2P Distributed Chat w Raft Consensus/headers/messages.hpp
@@ -12,18 +12,43 @@ class RumorMessage {
         int from;
         int seqNo;
 
+        /**
+         * @brief Decompose the message into from, chatText and seqNo
+         *      Message format: "from:___,chatText:___,seqNo:___"
+        */
         void decomposeMessage();
 
     public:
         RumorMessage();
+
+        /**
+         * @brief Construct a new Rumor Message:: Rumor Message object
+         * @param message Message received
+        */
         RumorMessage(std::string message);
 
         /*****************************
          * Getters                   *
          * ***************************/
-        std::string getMessage();
+
+        /**
+         * @brief Get the chatText from the message
+        */
         std::string getChatText();
+
+        /**
+         * @brief Get the message
+        */
+        std::string getMessage();
+
+        /**
+         * @brief Get the sender of the message
+        */
         int getFrom();
+
+        /**
+         * @brief Get the sequence number of the message
+        */
         int getSeqNo();
 };
 
@@ -34,22 +59,50 @@ class StatusMessage {
 
     public:
         StatusMessage();
+
+        /**
+         * @brief Construct a new Status Message:: Status Message object
+         * @param port Port number of the server
+        */
         StatusMessage(int port);
 
         /*****************************
          * Setters                   *
          * ***************************/
+
+        /**
+         * @brief Update the status of max messages at the other server
+         * @param port Port number of the other server
+         * @param seqNo Sequence number of the message
+        */
         void updateStatus(int port, int seqNo);
+
+        /**
+         * @brief Add a message to the global chat log of current server
+         * @param message Message to be added
+         * @param seq Sequence number of the message (also the max sequence number)
+        */
         void addMessageToLog(RumorMessage message, int seq);
-        void updateMaxSeqNo(int seqNo);
 
         /*****************************
          * Getters                   *
          * ***************************/
+
+        /**
+         * @brief Get the status of the other server
+        */
         std::map<int, int> getStatus();
+
+        /**
+         * @brief Get the chat log of the server until maxSeqNo elements
+        */
         std::vector<RumorMessage> getChatLog();
+
+        /**
+         * @brief Get a specific message from the chat log
+         * @param seqNo Sequence number of the message to retrieve
+        */
         std::string getMessage(int from, int seqNo);
-        int getMaxSeqNo();
 };
 
 #endif
--- P2P Distributed Chat w Raft Consensus/messages.cpp
+++ P2P Distributed Chat w Raft Consensus/messages.cpp
@@ -10,20 +10,11 @@ RumorMessage::RumorMessage() {
     this->from = -1;
 }
 
-/**
- * @brief Construct a new Rumor Message:: Rumor Message object
- * @param seq Sequence number of the message
- * @param message Message received
-*/
 RumorMessage::RumorMessage(std::string message) {
     this->message = message;
     this->decomposeMessage();
 }
 
-/**
- * @brief Decompose the message into from, chatText and seqNo
- *      Message format: "from:___,chatText:___,seqNo:___"
-*/
 void RumorMessage::decomposeMessage() {
     std::string delimiter = ",";
     std::string fromDelimiter = "from:";
@@ -45,30 +36,18 @@ void RumorMessage::decomposeMessage() {
     this->seqNo = std::stoi(seqNo);
 }
 
-/**
- * @brief Get the chatText from the message
-*/
 std::string RumorMessage::getChatText() {
     return this->chatText;
 }
 
-/**
- * @brief Get the message
-*/
 std::string RumorMessage::getMessage() {
     return this->message;
 }
 
-/**
- * @brief Get the sender of the message
-*/
 int RumorMessage::getFrom() {
     return this->from;
 }
 
-/**
- * @brief Get the sequence number of the message
-*/
 int RumorMessage::getSeqNo() {
     return this->seqNo;
 }
@@ -79,38 +58,20 @@ int RumorMessage::getSeqNo() {
 
 StatusMessage::StatusMessage() {}
 
-/**
- * @brief Construct a new Status Message:: Status Message object
- * @param port Port number of the server
-*/
 StatusMessage::StatusMessage(int port) {
     this->statusMap[port] = 0;
     this->chatLog = std::vector<RumorMessage>();
 }
 
-/**
- * @brief Update the status of max messages at the other server
- * @param port Port number of the other server
- * @param seqNo Sequence number of the message
-*/
 void StatusMessage::updateStatus(int port, int seqNo) {
     this->statusMap[port] = seqNo;
 }
 
-/**
- * @brief Add a message to the global chat log of current server
- * @param message Message to be added
- * @param seq Sequence number of the message (also the max sequence number)
-*/
 void StatusMessage::addMessageToLog(RumorMessage message, int seq) {
     std::cout << "Adding message " << seq << " to chat log" << std::endl;
-    // std::cout << "Current status map:" << std::endl;
-    // for (const auto& pair : this->statusMap) {
-    //     std::cout << "Key: " << pair.first << ", Value: " << pair.second << std::endl;
-    // }
     if (this->statusMap.find(message.getFrom()) == this->statusMap.end()) {
         if (seq == 1) {
-            this->statusMap[message.getFrom()] = 1;     
+            this->statusMap[message.getFrom()] = 1;
         } else {
             std::cout << "THIS IS BAD. Trying to write to new origin:" << message.getFrom() << " seq:" << seq << std::endl;
             return;
@@ -124,24 +85,14 @@ void StatusMessage::addMessageToLog(RumorMessage message, int seq) {
     this->chatLog.push_back(message);
 }
 
-/**
- * @brief Get the status of the other server
-*/
 std::map<int, int> StatusMessage::getStatus() {
     return this->statusMap;
 }
 
-/**
- * @brief Get the chat log of the server until maxSeqNo elements
-*/
 std::vector<RumorMessage> StatusMessage::getChatLog() {
     return this->chatLog;
 }
 
-/**
- * @brief Get a specific message from the chat log
- * @param seqNo Sequence number of the message to retrieve
-*/
 std::string StatusMessage::getMessage(int from, int seqNo) {
     for (RumorMessage message : this->chatLog) {
         if ((message.getFrom() == from) && (message.getSeqNo() == seqNo)) {
